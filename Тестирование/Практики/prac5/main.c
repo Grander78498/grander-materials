eaf965dad89e1072a373060034804a1a9878c614
Grander78498 <grander.l.l.1242@gmail.com>
2024-12-03 00:15:44 +0300

    Код на C для тивпо

--- Тестирование/Практики/prac5/main.c
+++ Тестирование/Практики/prac5/main.c
@@ -1,5 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+
+int read_number(int* n) {
+    char buffer[100];
+    char *endptr;
+
+    if (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        char *newline = strchr(buffer, '\n');
+        if (newline) {
+            *newline = '\0';
+        }
+
+        *n = strtol(buffer, &endptr, 10);
+
+        if (*endptr != '\0') {
+            printf("Ошибка: введенная строка содержит нечисловые символы\n");
+            return -1;
+        }
+        return 0;
+    } else {
+        printf("Ошибка ввода\n");
+        return -1;
+    }
+}
+
 int main()
 {
     int n, m;
@@ -7,11 +35,15 @@ int main()
     m = 0;
     while (n < 1) {
         printf("Введите высоту матрицы: ");
-        scanf("%d", &n);
+        if (read_number(&n) != 0) {
+            return 1;
+        };
     }
     while (m < 1) {
         printf("Введите ширину матрицы: ");
-        scanf("%d", &m);
+        if (read_number(&m) != 0) {
+            return 1;
+        };
     }
     int** a = (int**)malloc(n * sizeof(int*));
     if (a == NULL) {
@@ -30,7 +62,13 @@ int main()
     for (int i = 0; i < n; i++) {
         printf("Введите строку\n");
         for (int j = 0; j < m; j++) {
-            scanf("%d", &a[i][j]);
+            if (read_number(&a[i][j]) != 0) {
+                for (int j = 0; j < i; j++) {
+                    free(a[j]);
+                }
+                free(a);
+                return 1;
+            };
         }
     }
     printf("Результат ввода\n");
@@ -44,13 +82,9 @@ int main()
             }
         }
     }
-    getchar();
     n += 1;
     int** b = (int**)realloc(a, n * sizeof(int*));
     if (b == NULL) {
-        for (int j = 0; j < n; j++) {
-            free(a[j]);
-        }
         free(a);
         return -1;
     }
@@ -70,7 +104,13 @@ int main()
     }
     printf("Введите строку для вставки\n");
     for (int j = 0; j < m; j++) {
-        scanf("%d", &a[0][j]);
+        if (read_number(&a[0][j]) != 0) {
+            for (int i = 0; i < n; j++) {
+                free(a[i]);
+            }
+            free(a);
+            return 1;
+        };
     }
     printf("Результат выполнения\n");
     for (int i = 0; i < n; i++) {
